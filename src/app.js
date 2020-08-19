0e18bf9cbd39cb4c8c6beab67141a010aab9e0d5
Hesron <hesronbressiani@hotmail.com>
2020-08-19 00:12:32 -0300

    post/repositories

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -15,7 +15,14 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  var likes = 0;
+
+  const repository = { id: uuid(), title, url, techs, likes}
+
+  repositories.push(repository)
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
